e57aaf257b6835a0ca6cb43e17e36087d0ce441f
jas3333 <accounts1@protonmail.com>
2022-09-24 11:28:17 -0700

    Messing with git

--- javascript/8_kyu/personal_message.js
+++ javascript/8_kyu/personal_message.js
@@ -2,5 +2,5 @@
 // Function takes two parameters: name, owner
 const greet = (name, owner) => name === owner ? "Hello boss" : "Hello guest";
 
-console.log(greet("Theo", "Theo"));
+console.log(greet("Ollie", "Ollie"));
 console.log(greet("Theo", "Ollie"));
