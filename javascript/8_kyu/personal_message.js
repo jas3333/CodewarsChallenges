f8095ea345e8a1b8d2ed5ecd5711dfc8bdac670b
jas3333 <accounts1@protonmail.com>
2022-09-24 11:09:02 -0700

    Create a function that gives personalized message

---
+++ javascript/8_kyu/personal_message.js
@@ -0,0 +1,6 @@
+// Create a function that gives a personalized greeting...
+// Function takes two parameters: name, owner
+const greet = (name, owner) => name === owner ? "Hello boss" : "Hello guest";
+
+console.log(greet("Theo", "Theo"));
+console.log(greet("Theo", "Ollie"));
